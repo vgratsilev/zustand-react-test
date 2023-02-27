4804d0ccd3632281cd78bda80225839ab6b0c20b
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2023-02-27 16:25:21 +0500

    Project setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root').render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <ChakraProvider>
         <App />
-    </ChakraProvider>
-))
+    </ChakraProvider>,
+);
